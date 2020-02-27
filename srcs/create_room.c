28eb62f07903bc7acc544898b3f5e2263265c835
Moana-rose Marcello <mmarcell@f1r2s16.codam.nl>
2020-02-27 14:17:01 +0100

    add create_room()

--- srcs/create_room.c
+++ srcs/create_room.c
@@ -6,7 +6,7 @@
 /*   By: mmarcell <mmarcell@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/26 14:46:04 by mmarcell       #+#    #+#                */
-/*   Updated: 2020/02/26 16:48:40 by mmarcell      ########   odam.nl         */
+/*   Updated: 2020/02/27 14:13:30 by mmarcell      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 t_room		*create_room(char **room_info)
 {
-	
+	t_room		*room;
+
+	room = (t_room*)malloc(sizeof(t_room));
+	if (room == 0 || room_info == 0 || *room_info == 0 ||
+		room_info[0][0] == 'L' || room_info[0][0] == '#' ||
+		room_info[1] == 0 || ft_is_int(room_info[1]) == 0 ||
+		room_info[2] == 0 || ft_is_int(room_info[2]) == 0 ||
+		room_info[3] != 0)
+		return (0);
+	room->name = ft_strcpy(room_info[0]);
+	room->x_coord = ft_atoi(room_info[1]);
+	room->y_coord = ft_atoi(room_info[2]);
+	room->distance = -1;
+	room->link_count = 0;
+	room->links = 0;
+	ft_strarrdel(&room_info);
+	return (room);
 }
