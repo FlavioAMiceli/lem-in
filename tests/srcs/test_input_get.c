d0a18c3a5f7c4e9d5b3c9db6c911e01dc8470149
mocello <mmarcell@student.codam.nl>
2020-03-23 11:21:57 +0100

    delete all hmap files so program is compilable

--- tests/srcs/test_input_get.c
+++ tests/srcs/test_input_get.c
@@ -6,7 +6,7 @@
 /*   By: mmarcell <mmarcell@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/04 15:09:27 by mmarcell       #+#    #+#                */
-/*   Updated: 2020/03/05 12:02:20 by mmarcell      ########   odam.nl         */
+/*   Updated: 2020/03/23 11:20:45 by moana         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ Test(read_input, valid_01)
 	t_input_info	input;
 
 	ft_bzero(&input, sizeof(input));
-	fd = open("./maps/01", O_RDONLY);
+	fd = open("maps/01", O_RDONLY);
   	if (fd == -1)
    	 ft_printf ("Error\n %s\n", strerror(errno));
-	cr_assert_gt(fd, 0, "fd is smaller than zero");
+	cr_assert_gt(fd, 0, "open failed, fd is %d", fd);
 	dup2(fd, 0);
 	cr_assert_eq(read_input(&input), 1, "reading normal map doesn't return 1");
 }
