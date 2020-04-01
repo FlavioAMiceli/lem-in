bf0dbbabd8a88fbea8549a238f615fbce5acf73f
mocello <mmarcell@student.codam.nl>
2020-04-01 18:18:46 +0200

    fix segfault when trying to get an item that doesn't exist

--- srcs/hmap_get.c
+++ srcs/hmap_get.c
@@ -6,7 +6,7 @@
 /*   By: fmiceli <fmiceli@student.codam.nl...>        +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/09 21:18:24 by fmiceli        #+#    #+#                */
-/*   Updated: 2020/03/30 17:51:58 by moana         ########   odam.nl         */
+/*   Updated: 2020/04/01 18:17:16 by moana         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,15 @@ void    *hmap_get(t_hmap *hmap, char *key)
 	hash = hmap_hash(key, hmap->n);
 	i = hash % hmap->n;
 	checked = 0;
-	while (!ft_strequ(hmap->slots[i]->key, key) && checked <= hmap->n)
+	while (!(hmap->slots[i]) && !ft_strequ(hmap->slots[i]->key, key)
+		&& checked <= hmap->n)
 	{
 		i = (((5 * i) + 1) + hash) % hmap->n;
 		hash >>= PERTURB_SHIFT;
 		if (hash == 0)
 			checked++;
 	}
-	if (!ft_strequ(hmap->slots[i]->key, key))
+	if (!(hmap->slots[i]) || !ft_strequ(hmap->slots[i]->key, key))
 		return (NULL);
 	return (hmap->slots[i]->val);
 }
