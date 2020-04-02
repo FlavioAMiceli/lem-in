d86036f82df691178d0bbe7d16a1803100adf0b4
mocello <mmarcell@student.codam.nl>
2020-04-02 17:47:17 +0200

    fix bug in while condition

--- srcs/hmap_get.c
+++ srcs/hmap_get.c
@@ -6,7 +6,7 @@
 /*   By: fmiceli <fmiceli@student.codam.nl...>        +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/09 21:18:24 by fmiceli        #+#    #+#                */
-/*   Updated: 2020/04/01 18:17:16 by moana         ########   odam.nl         */
+/*   Updated: 2020/04/02 17:44:47 by moana         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void    *hmap_get(t_hmap *hmap, char *key)
 	hash = hmap_hash(key, hmap->n);
 	i = hash % hmap->n;
 	checked = 0;
-	while (!(hmap->slots[i]) && !ft_strequ(hmap->slots[i]->key, key)
+	while (hmap->slots[i] != NULL && !ft_strequ(hmap->slots[i]->key, key)
 		&& checked <= hmap->n)
 	{
 		i = (((5 * i) + 1) + hash) % hmap->n;
