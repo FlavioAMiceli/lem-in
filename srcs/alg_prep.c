76e58771d1b4e2c14dcee367454c0af71d620a81
mocello <mmarcell@student.codam.nl>
2020-04-09 13:04:33 +0200

    finish distance_set() which works as expected now. Deadends are not yet detected. Add new testsmaps

--- srcs/alg_prep.c
+++ srcs/alg_prep.c
@@ -6,12 +6,30 @@
 /*   By: moana <moana@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/07 15:51:21 by moana         #+#    #+#                 */
-/*   Updated: 2020/04/08 18:53:05 by moana         ########   odam.nl         */
+/*   Updated: 2020/04/09 12:57:46 by moana         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 #include "libft.h"
+
+static int	is_deadend(t_graph *graph, t_vert *vert, t_vert *prev_vert)
+{
+	t_edge	*edge;
+
+	edge = vert->connections;
+	while (edge != NULL)
+	{
+		if (edge->head != prev_vert)
+		{
+			if (edge->head == graph->source || edge->head == graph->sink
+				|| edge->head->distance != -1)
+				return (FALSE);
+		}
+		edge = edge->next_conn;
+	}
+	return (TRUE);
+}
 
 int		distance_set(t_graph *graph, t_vert *vert, t_vert *prev_vert)
 {
@@ -20,20 +38,25 @@ int		distance_set(t_graph *graph, t_vert *vert, t_vert *prev_vert)
 	if (vert == NULL || (vert->conn_count == 1
 		&& vert != graph->sink && vert != graph->source))
 		return (ERROR);
-	if (vert->distance > 0 || vert == graph->source)
+	if ((prev_vert != NULL
+		&& vert->distance != -1 && vert->distance <= prev_vert->distance)
+		|| vert == graph->source)
 		return (OK);
 	if (vert == graph->sink)
 		vert->distance = 0;
-	else if (vert != graph->sink && prev_vert != NULL && vert->distance == -1)
-		vert->distance = ++(prev_vert->distance);
+	else if (vert != graph->sink && prev_vert != NULL)
+		vert->distance = prev_vert->distance + 1;
 	edge = vert->connections;
 	while (edge != NULL)
 	{
-		if (edge->head->distance == -1 && edge->head != prev_vert)
-			distance_set(graph, edge->head, vert);
+		if (edge->head != prev_vert && edge->head != graph->sink
+			&& distance_set(graph, edge->head, vert) == OK && vert != graph->sink
+			&& edge->next_conn == NULL)
+			return (OK);
 		edge = edge->next_conn;
-	}
-	if (vert->distance < 0)
-		return (ERROR);
+	}	
+	if (vert != graph->source && vert != graph->sink
+		&& is_deadend(graph, vert, prev_vert) == TRUE)
+		vert->distance = -1;
 	return (OK);
 }
