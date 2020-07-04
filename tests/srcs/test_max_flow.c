2bb6b70e3ddfb32c0c3a070c42731b604180a768
Moana-rose Marcello <mmarcell@student.codam.nl>
2020-07-04 19:06:59 +0200

    add tests for edmonds_karp()

--- tests/srcs/test_max_flow.c
+++ tests/srcs/test_max_flow.c
@@ -6,7 +6,7 @@
 /*   By: mmarcell <mmarcell@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/04 16:07:35 by mmarcell      #+#    #+#                 */
-/*   Updated: 2020/07/04 17:55:50 by mmarcell      ########   odam.nl         */
+/*   Updated: 2020/07/04 19:04:59 by mmarcell      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,9 +52,11 @@ Test(max_flow, valid_normal)
 	distance_set(&graph, graph.sink, NULL);
 	edmonds_karp(&graph);
 	cr_assert_eq(graph.path_count, paths, "number of paths %d doesn't match expected number of paths %d", graph.path_count, paths);
+
 	cr_assert_eq(graph.paths[0]->start, path_1, "first path saved: %s is not equal to first path expected: %s\n", graph.paths[0]->start->name, path_1->name);
 	cr_assert_eq(graph.paths[0]->start->hops, hops_1, "first path hops saved: %d is not equal to first path hops expected: %d\n", graph.paths[0]->start->hops, hops_1);
 	cr_assert_eq(graph.paths[0]->threshold, thresh_1, "threshold %d for first path doesn't match expected threshold %d\n", graph.paths[0]->threshold, thresh_1);
+
 	graph_del(&graph);
 }
 
@@ -73,9 +75,11 @@ Test(max_flow, valid_normal_2_ants_1)
 	distance_set(&graph, graph.sink, NULL);
 	edmonds_karp(&graph);
 	cr_assert_eq(graph.path_count, paths, "number of paths %d doesn't match expected number of paths %d", graph.path_count, paths);
+
 	cr_assert_eq(graph.paths[0]->start, path_1, "first path saved: %s is not equal to first path expected: %s\n", graph.paths[0]->start->name, path_1->name);
 	cr_assert_eq(graph.paths[0]->start->hops, hops_1, "first path hops saved: %d is not equal to first path hops expected: %d\n", graph.paths[0]->start->hops, hops_1);
 	cr_assert_eq(graph.paths[0]->threshold, thresh_1, "threshold %d for first path doesn't match expected threshold %d\n", graph.paths[0]->threshold, thresh_1);
+
 	graph_del(&graph);
 }
 
@@ -98,11 +102,84 @@ Test(max_flow, valid_normal_2_ants_2)
 	distance_set(&graph, graph.sink, NULL);
 	edmonds_karp(&graph);
 	cr_assert_eq(graph.path_count, paths, "number of paths %d doesn't match expected number of paths %d", graph.path_count, paths);
+
 	cr_assert_eq(graph.paths[0]->start, path_1, "first path saved: %s is not equal to first path expected: %s\n", graph.paths[0]->start->name, path_1->name);
 	cr_assert_eq(graph.paths[0]->start->hops, hops_1, "first path hops saved: %d is not equal to first path hops expected: %d\n", graph.paths[0]->start->hops, hops_1);
 	cr_assert_eq(graph.paths[0]->threshold, thresh_1, "threshold %d for first path doesn't match expected threshold %d\n", graph.paths[0]->threshold, thresh_1);
-	cr_assert_eq(graph.paths[1]->start, path_2, "first path saved: %s is not equal to first path expected: %s\n", graph.paths[0]->start->name, path_2->name);
-	cr_assert_eq(graph.paths[1]->start->hops, hops_2, "first path hops saved: %d is not equal to first path hops expected: %d\n", graph.paths[0]->start->hops, hops_2);
-	cr_assert_eq(graph.paths[1]->threshold, thresh_2, "threshold %d for first path doesn't match expected threshold %d\n", graph.paths[0]->threshold, thresh_2);
+
+	cr_assert_eq(graph.paths[1]->start, path_2, "second path saved: %s is not equal to second path expected: %s\n", graph.paths[1]->start->name, path_2->name);
+	cr_assert_eq(graph.paths[1]->start->hops, hops_2, "first path hops saved: %d is not equal to first path hops expected: %d\n", graph.paths[1]->start->hops, hops_2);
+	cr_assert_eq(graph.paths[1]->threshold, thresh_2, "threshold %d for first path doesn't match expected threshold %d\n", graph.paths[1]->threshold, thresh_2);
+
+	graph_del(&graph);
+}
+
+Test(max_flow, valid_normal_2_ants_4)
+{
+	t_graph		graph;
+
+	cr_assert_eq(load_map(&graph, "tests/maps/valid_deadend_4"), SUCCESS, "graph couldn't be build");
+
+	unsigned int	paths = 2;
+	t_vert			*path_1 = hmap_get(graph.vertices, "D");
+	int				hops_1 = 3;
+	unsigned int	thresh_1 = 1;
+	cr_assert_neq(path_1, NULL, "couldn't find vertix");
+	t_vert			*path_2 = hmap_get(graph.vertices, "G");
+	int				hops_2 = 3;
+	unsigned int	thresh_2 = 2;
+	cr_assert_neq(path_2, NULL, "couldn't find vertix");
+
+	distance_set(&graph, graph.sink, NULL);
+	edmonds_karp(&graph);
+	cr_assert_eq(graph.path_count, paths, "number of paths %d doesn't match expected number of paths %d", graph.path_count, paths);
+
+	cr_assert_eq(graph.paths[0]->start, path_1, "first path saved: %s is not equal to first path expected: %s\n", graph.paths[0]->start->name, path_1->name);
+	cr_assert_eq(graph.paths[0]->start->hops, hops_1, "first path hops saved: %d is not equal to first path hops expected: %d\n", graph.paths[0]->start->hops, hops_1);
+	cr_assert_eq(graph.paths[0]->threshold, thresh_1, "threshold %d for first path doesn't match expected threshold %d\n", graph.paths[0]->threshold, thresh_1);
+
+	cr_assert_eq(graph.paths[1]->start, path_2, "second path saved: %s is not equal to second path expected: %s\n", graph.paths[1]->start->name, path_2->name);
+	cr_assert_eq(graph.paths[1]->start->hops, hops_2, "first path hops saved: %d is not equal to first path hops expected: %d\n", graph.paths[1]->start->hops, hops_2);
+	cr_assert_eq(graph.paths[1]->threshold, thresh_2, "threshold %d for first path doesn't match expected threshold %d\n", graph.paths[1]->threshold, thresh_2);
+
+	graph_del(&graph);
+}
+
+Test(max_flow, valid_normal_2_ants_5)
+{
+	t_graph		graph;
+
+	cr_assert_eq(load_map(&graph, "tests/maps/valid_deadend_4"), SUCCESS, "graph couldn't be build");
+
+	unsigned int	paths = 3;
+	t_vert			*path_1 = hmap_get(graph.vertices, "D");
+	int				hops_1 = 4;
+	unsigned int	thresh_1 = 1;
+	cr_assert_neq(path_1, NULL, "couldn't find vertix");
+	t_vert			*path_2 = hmap_get(graph.vertices, "G");
+	int				hops_2 = 4;
+	unsigned int	thresh_2 = 2;
+	cr_assert_neq(path_2, NULL, "couldn't find vertix");
+	t_vert			*path_3 = hmap_get(graph.vertices, "G");
+	int				hops_3 = 4;
+	unsigned int	thresh_3 = 3;
+	cr_assert_neq(path_3, NULL, "couldn't find vertix");
+
+	distance_set(&graph, graph.sink, NULL);
+	edmonds_karp(&graph);
+	cr_assert_eq(graph.path_count, paths, "number of paths %d doesn't match expected number of paths %d", graph.path_count, paths);
+
+	cr_assert_eq(graph.paths[0]->start, path_1, "first path saved: %s is not equal to first path expected: %s\n", graph.paths[0]->start->name, path_1->name);
+	cr_assert_eq(graph.paths[0]->start->hops, hops_1, "first path hops saved: %d is not equal to first path hops expected: %d\n", graph.paths[0]->start->hops, hops_1);
+	cr_assert_eq(graph.paths[0]->threshold, thresh_1, "threshold %d for first path doesn't match expected threshold %d\n", graph.paths[0]->threshold, thresh_1);
+
+	cr_assert_eq(graph.paths[1]->start, path_2, "second path saved: %s is not equal to second path expected: %s\n", graph.paths[1]->start->name, path_2->name);
+	cr_assert_eq(graph.paths[1]->start->hops, hops_2, "first path hops saved: %d is not equal to first path hops expected: %d\n", graph.paths[1]->start->hops, hops_2);
+	cr_assert_eq(graph.paths[1]->threshold, thresh_2, "threshold %d for first path doesn't match expected threshold %d\n", graph.paths[1]->threshold, thresh_2);
+
+	cr_assert_eq(graph.paths[2]->start, path_3, "second path saved: %s is not equal to second path expected: %s\n", graph.paths[2]->start->name, path_3->name);
+	cr_assert_eq(graph.paths[2]->start->hops, hops_3, "first path hops saved: %d is not equal to first path hops expected: %d\n", graph.paths[2]->start->hops, hops_3);
+	cr_assert_eq(graph.paths[2]->threshold, thresh_3, "threshold %d for first path doesn't match expected threshold %d\n", graph.paths[2]->threshold, thresh_3);
+
 	graph_del(&graph);
 }
