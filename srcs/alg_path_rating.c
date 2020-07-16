e69d7830181b5c22973541ad8c5014c5cdd61fd6
moana <moanamarcello@gmail.com>
2020-07-16 16:45:51 +0200

    add first version of path_rating() which only checks whether a path threshold is above the ant_total

---
+++ srcs/alg_path_rating.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   alg_path_rating.c                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mmarcell <mmarcell@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/07/16 16:28:54 by mmarcell      #+#    #+#                 */
+/*   Updated: 2020/07/16 16:28:54 by mmarcell      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+#include "libft.h"
+
+int		path_rating(t_path **paths, unsigned int path_count, \
+		unsigned int ant_total)
+{
+	while (path_count > 0)
+	{
+		if (paths[path_count]->threshold > ant_total)
+			return (ERROR);
+		--path_count;
+	}
+	return (SUCCESS);
+}
